a07447307a76850f5ac75c556c222e3a284953c6
Jakub Cybulski <jakub_cybulski@o2.pl>
2025-10-17 12:54:30 +0200

    Task StreamPractice from Java 8 — cz. 2

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,34 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    private int getTimeInUkraine(String period) {
+        if (period != null) {
+            String[] dates = period.split("-");
+            if (dates.length == 2) {
+                int from = Integer.parseInt(dates[0]);
+                int to = Integer.parseInt(dates[1]);
+                return to - from;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() < 35) {
+            return false;
+        }
+        if (!candidate.getNationality().equals("Ukrainian")) {
+            return false;
+        }
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+        return getTimeInUkraine(candidate.getPeriodsInUkr()) >= 10;
+    }
 }
